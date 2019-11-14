45b9c627879aaf1c0bfd74db7638a1df6cd8bc53
adarshrh <adarshrh004@gmail.com>
2019-11-14 01:52:33 -0600

    Adding new spring boot project files

---
+++ wpl/src/main/java/project/wpl/controller/UserRegistrationController.java
@@ -0,0 +1,27 @@
+package project.wpl.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import project.wpl.model.UserRegistry;
+import project.wpl.response.UserRegistrationResponse;
+
+import javax.validation.Valid;
+import java.util.Map;
+
+
+@RestController
+public class UserRegistrationController {
+
+    private static Logger logger = LoggerFactory.getLogger(UserRegistrationController.class);
+
+    @PostMapping(value =  "/userRegistration", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity createDataSet(@Valid @RequestBody UserRegistry userRegistry,  @RequestParam Map<String, String> params) throws Exception {
+        //TODO read request body and process
+        UserRegistrationResponse registrationResponse = new UserRegistrationResponse();
+        return new ResponseEntity(registrationResponse, HttpStatus.ACCEPTED);
+    }
+}
