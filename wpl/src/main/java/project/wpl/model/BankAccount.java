8a7eaccc39e6f9f812ae990a35a433c174f3a9f6
akash-akki <akashpakki@gmail.com>
2019-11-14 23:32:26 -0600

    Added BankAccount Bean Class

---
+++ wpl/src/main/java/project/wpl/model/BankAccount.java
@@ -0,0 +1,45 @@
+package project.wpl.model;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+
+@Entity
+@Table(name = "BankAccount")
+public class BankAccount {
+    private String accountNumber;
+    private String routingNumber;
+    private String username;
+    private Double accountBalance;
+   
+    @Id
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public String getRoutingNumber() {
+		return routingNumber;
+	}
+	public void setRoutingNumber(String routingNumber) {
+		this.routingNumber = routingNumber;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public Double getAccountBalance() {
+		return accountBalance;
+	}
+	public void setAccountBalance(Double accountBalance) {
+		this.accountBalance = accountBalance;
+	}
+	
+
+}
