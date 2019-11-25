68cdac4f1a80545fa2ecc92d5396d1d9ead7daa0
adarshrh <adarshrh004@gmail.com>
2019-11-25 00:12:37 -0600

    Files to simulate weekly stock

---
+++ wpl/src/main/java/project/wpl/simulation/Stocks.java
@@ -0,0 +1,16 @@
+package project.wpl.simulation;
+
+import java.util.List;
+
+public class Stocks {
+
+    List<SingleStock> stocksList;
+
+    public List<SingleStock> getStocksList() {
+        return stocksList;
+    }
+
+    public void setStocksList(List<SingleStock> stocksList) {
+        this.stocksList = stocksList;
+    }
+}
