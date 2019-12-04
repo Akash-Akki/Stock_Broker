0f69e1f1a9d0fda7d339ed80a27fe3e8d1ab0779
akash-akki <akashpakki@gmail.com>
2019-12-04 14:27:57 -0600

    Buy bug fixed

--- wpl/src/main/java/project/wpl/service/UserDetailServiceImpl.java
+++ wpl/src/main/java/project/wpl/service/UserDetailServiceImpl.java
@@ -117,42 +117,53 @@ public class UserDetailServiceImpl implements UserDetailsService {
         bankAccountRepository.save(findByAccountNumber.get());
         List<UserShare> findUserShareBySymbol = userShareRepository.findByUsername(buyStock.getUsername());
        UserShare userShare=new UserShare();
+       boolean flag=false;
        if(findUserShareBySymbol.size()!=0) {
            for (int i = 0; i < findUserShareBySymbol.size(); i++) {
+
                //System.out.println("username is "+findUserShareBySymbol.get(i).getUsername());
               // System.out.println("company is "+findUserShareBySymbol.get(i).getCompany());
 
                String symbol = findUserShareBySymbol.get(i).getSymbol();
 
+
                System.out.println("symbol is "+symbol);
                if (buyStock.getSymbol().equals(symbol)) {
+                   Long u = findUserShareBySymbol.get(i).getId();
                    int quantity = findUserShareBySymbol.get(i).getQuantity();
                    int updatedQuantity = quantity + buyStock.getNumberOfUnits();
                 //   System.out.println("updated quantity is "+updatedQuantity);
-
                    findUserShareBySymbol.get(i).setQuantity(updatedQuantity);
                    userShareRepository.save(findUserShareBySymbol.get(i));
-               }
-               else{
-                     userShare.setSymbol(buyStock.getSymbol());
-                     userShare.setCompany(buyStock.getCompany_name());
-                     userShare.setQuantity(buyStock.getNumberOfUnits());
-                     userShare.setCompany(buyStock.getCompany_name());
-                     userShareRepository.save(userShare);
+                   System.out.println("flag true");
+                   flag=true;
+                   break;
                }
 
            }
        }
-       else{
+       if(flag==false){
+
            userShare.setSymbol(buyStock.getSymbol());
            userShare.setUsername(buyStock.getUsername());
            userShare.setQuantity(buyStock.getNumberOfUnits());
            userShare.setCompany(buyStock.getCompany_name());
-          // findUserShareBySymbol.add(userShare);
-           System.out.println("before saving");
+           // findUserShareBySymbol.add(userShare);
+
+           System.out.println("before saving flag false");
            userShareRepository.save(userShare);
 
        }
+//       else{
+//           userShare.setSymbol(buyStock.getSymbol());
+//           userShare.setUsername(buyStock.getUsername());
+//           userShare.setQuantity(buyStock.getNumberOfUnits());
+//           userShare.setCompany(buyStock.getCompany_name());
+//          // findUserShareBySymbol.add(userShare);
+//           System.out.println("before saving");
+//           userShareRepository.save(userShare);
+//
+//       }
 //       if(findUserShareBySymbol != null)
 //       {
 //                      int quantity=findUserShareBySymbol.getQuantity();
