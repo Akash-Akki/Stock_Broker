bba8d467176d1d3cfaca172897b545c1a02f9d58
akash-akki <akashpakki@gmail.com>
2019-12-04 02:05:34 -0600

    Cookie Buy and etc.

--- wpl/src/main/java/project/wpl/kafka/Sender.java
+++ wpl/src/main/java/project/wpl/kafka/Sender.java
@@ -4,16 +4,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
-@Service
-public class Sender {
-
-
-
-    @Autowired
-    private KafkaTemplate<String, String> kafkaTemplate;
-
-    public void send(String payload) {
-       System.out.println(payload);
-        kafkaTemplate.send("wpl", payload);
-    }
-}
+//@Service
+//public class Sender {
+//
+//
+//
+//    @Autowired
+//    private KafkaTemplate<String, String> kafkaTemplate;
+//
+//    public void sendBuy(String payload) {
+//       System.out.println(payload);
+//        kafkaTemplate.send("buywpl", payload);
+//    }
+//
+//    public void sendSell(String payload) {
+//        System.out.println(payload);
+//        kafkaTemplate.send("sellwpl", payload);
+//    }
+//
+//}
